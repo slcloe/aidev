d7a5072eb7ba77622af3d14797f3af5167bbd639
slcloe <gkstmf1403@naver.com>
2024-09-26 11:23:56 +0900

    init

---
+++ src/main/java/kr/co/shinhands/edu/aidev/App.java
@@ -0,0 +1,15 @@
+package kr.co.shinhands.edu.aidev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for (int i = 0; i < 10; i++) {
+            System.out.println( "Hello World!" );
+        }
+    }
+}
