58c698d734fe5a205f726370e5f9d0f91e03e15a
slcloe <gkstmf1403@naver.com>
2024-09-26 11:28:31 +0900

    refactor

--- src/main/java/kr/co/shinhands/edu/aidev/App.java
+++ src/main/java/kr/co/shinhands/edu/aidev/App.java
@@ -1,15 +1,13 @@
 package kr.co.shinhands.edu.aidev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        sayhello("Hello World!");
+    }
+
+    private static void sayhello(String message) {
         for (int i = 0; i < 10; i++) {
-            System.out.println( "Hello World!" );
+            System.out.println(message);
         }
     }
 }
